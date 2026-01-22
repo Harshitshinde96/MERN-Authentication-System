6007df80c2615f6c304f07ba0b14fcade2af3919
Harshit Shinde <harshitshinde65@gmail.com>
2026-01-22 20:52:34 +0530

    Working on nodemailer ishue

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT, // 587
-  secure: false, // true for 465, false for other ports
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_USERNAME,
     pass: process.env.SMTP_PASSWORD,
