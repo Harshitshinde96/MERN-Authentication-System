fc4335997c497545baea9c01a181ece14de46e9a
Harshit Shinde <harshitshinde65@gmail.com>
2026-01-22 21:21:59 +0530

    Working on nodemailer ishue

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,29 +1,13 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-
-  // --- HARDCODED SETTINGS (To rule out Env Var errors) ---
-  port: 465,
-  secure: true,
-
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // Brevo requires this to be FALSE for port 587
   auth: {
     user: process.env.SMTP_USERNAME,
     pass: process.env.SMTP_PASSWORD,
   },
-
-  // --- NETWORK FIXES FOR RENDER ---
-  // 1. Force IPv4: This is the #1 fix for hanging connections
-  family: 4,
-
-  // 2. Increase Timeouts: Give the server 30s instead of 10s
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
-
-  // 3. Debugging: Keep logs on so we can see what happens
-  logger: true,
-  debug: true,
 });
 
 export default transporter;
